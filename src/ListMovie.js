617cc5b098e2e56b8b745bf49b48cc51bd5a0cdf
Leonardo Morini <leo.morini.nn@gmail.com>
2018-10-10 01:27:51 +0000

    first commit

---
+++ src/ListMovie.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class ListMovie extends Component {
+  render() {
+    const { profiles, users, movies } = this.props;
+ 
+    
+    
+    	
+    return (
+     	<h1>teste</h1>
+    );
+  }
+}
